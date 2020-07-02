c5f7e28d1b857f89decd722098f035168691ff5f
tirthamoulib <btirthamouli@gmail.com>
2020-07-02 13:37:17 +0530

    BST

--- BinarySearchTree/main.go
+++ BinarySearchTree/main.go
@@ -1,16 +1,19 @@
 package main
 
-import "fmt"
+import (
+	bst "github.com/Boom0027/go_data_structures/BinarySearchTree/lib"
+)
 
 func main() {
-	a := &node{
-		val: 23,
+	a := &bst.Node{
+		Val:   23,
+		Left:  nil,
+		Right: nil,
 	}
 
 	// Insert all the nodes
 	vals := []int{2, 45, 1, 21, 34, 123, 22, 15, 9, 96, 45}
 	for _, val := range vals {
-		fmt.Println(a.insert(val))
+		a.Insert(val)
 	}
-
 }
